9c2df39b5300d08a41be50bedfd5aca54810fa95
Neelam <neelammaqsood19@gmail.com>
2026-03-12 14:30:00 +0500

    Frontend: Performance optimization

--- models/course.model.js
+++ models/course.model.js
@@ -51,4 +51,4 @@ const courseSchema = new mongoose.Schema({
 }, {timestamps:true});
 
 export const Course = mongoose.model("Course", courseSchema);
-//this is cuse model assfsdfgdwew
+//this is cuse model assfsdfgdwewsderewert
